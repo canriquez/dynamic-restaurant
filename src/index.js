6f23c734679a46bb23d41610b7f4f0ae5fcd584b
Carlos Anriquez <canriquez@gmail.com>
2020-06-29 19:25:20 -0300

    add basic html/css, webpack loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './style.css'
+import navIcon from './burger.svg';
 console.log("init webpack done")
+
+// Navigation
+
+const navIconElement = document.getElementById('burger-menu')
+const navMenuIcon = new Image();
+navMenuIcon.src = navIcon;
+navIconElement = appendChild(navMenuIcon);
