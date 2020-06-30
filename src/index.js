307978708ba21e47142144f8313ae7f4983f344f
Carlos Anriquez <canriquez@gmail.com>
2020-06-30 14:39:12 -0300

    add main-section html tags. hero-image and brand-container

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import './style.css'
 import navIcon from './burger.svg';
+import brandLogo from './dp_logo.svg';
+import globalLogo from './globe.svg';
+
+// Main images import
+import main01 from './photo/main_01.jpg'
+
 console.log("init webpack done")
 
 // Navigation
 
 const navMenuIcon = new Image();
+const navBrand = new Image();
+const navGlobal = new Image();
 navMenuIcon.src = navIcon;
+navBrand.src = brandLogo;
+navGlobal.src = globalLogo;
 document.getElementById('burger-menu').appendChild(navMenuIcon);
+document.getElementById('brand-nav').appendChild(navBrand);
+document.getElementById('global').appendChild(navGlobal);
+
+//Main Section
+const mainHero01 = new Image();
+mainHero01.src = main01;
+document.getElementById('hero-image').appendChild(mainHero01);
+
