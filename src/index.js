fdb3264ad77275e246f0ff346f813c01bd10493d
Carlos Anriquez <canriquez@gmail.com>
2020-06-30 18:12:54 -0300

    create dish card html/css

--- src/index.js
+++ src/index.js
@@ -14,9 +14,20 @@ import master from './icons/mastercard.svg'
 import paypal from './icons/paypal.svg'
 import stripe from './icons/stripe.svg'
 
+// Dish Images
+import d01 from './photo/d01.jpg'
+import d02 from './photo/d02.jpg'
+import d03 from './photo/d03.jpg'
+import d04 from './photo/d04.jpg'
+import d05 from './photo/d05.jpg'
+import d06 from './photo/d06.jpg'
+import d07 from './photo/d07.jpg'
+
+
 console.log("init webpack done")
 
 // Navigation
+
 
 const navMenuIcon = new Image();
 const navBrand = new Image();
@@ -51,3 +62,17 @@ document.getElementById('payment-tags').appendChild(paypalIcon);
 document.getElementById('payment-tags').appendChild(visaIcon);
 document.getElementById('payment-tags').appendChild(masterIcon);
 document.getElementById('payment-tags').appendChild(stripeIcon);
+
+//Dish images
+const d01Icon = new Image();
+const d02Icon = new Image();
+const d03Icon = new Image();
+const d04Icon = new Image();
+d01Icon.src = d01;
+d02Icon.src = d02;
+d03Icon.src = d03;
+d04Icon.src = d04;
+document.getElementById('d01').appendChild(d01Icon);
+document.getElementById('d02').appendChild(d02Icon);
+document.getElementById('d03').appendChild(d03Icon);
+document.getElementById('d04').appendChild(d04Icon);
