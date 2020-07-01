a9ffaa60fa88904853eb8be2c18725726d9ae661
Carlos Anriquez <canriquez@gmail.com>
2020-07-01 12:54:51 -0300

    add basic cart item html/css

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ document.getElementById('payment-tags').appendChild(visaIcon);
 document.getElementById('payment-tags').appendChild(masterIcon);
 document.getElementById('payment-tags').appendChild(stripeIcon);
 
-//Dish images
+//Dish images menu & cart
 const d01Icon = new Image();
 const d02Icon = new Image();
 const d03Icon = new Image();
@@ -71,6 +71,7 @@ const d04Icon = new Image();
 const d05Icon = new Image();
 const d06Icon = new Image();
 const d07Icon = new Image();
+const d07cIcon = new Image();
 d01Icon.src = d01;
 d02Icon.src = d02;
 d03Icon.src = d03;
@@ -78,6 +79,8 @@ d04Icon.src = d04;
 d05Icon.src = d05;
 d06Icon.src = d06;
 d07Icon.src = d07;
+d07cIcon.src = d07;
+
 document.getElementById('d01').appendChild(d01Icon);
 document.getElementById('d02').appendChild(d02Icon);
 document.getElementById('d03').appendChild(d03Icon);
@@ -85,3 +88,5 @@ document.getElementById('d04').appendChild(d04Icon);
 document.getElementById('d05').appendChild(d05Icon);
 document.getElementById('d06').appendChild(d06Icon);
 document.getElementById('d07').appendChild(d07Icon);
+document.getElementById('d07c').appendChild(d07cIcon);
+
